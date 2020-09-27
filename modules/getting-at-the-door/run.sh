9edf6fc9961b358cc9511fa633abdc4b2bcb88e5
Gleb Ostrowski <glost@mailbox.org>
2020-09-27 13:36:09 +0200

    feat(dockerprtwo): ✨ Fix COPY to wrong containers
    - Attempt to fix ttyd compose startup

--- modules/getting-at-the-door/run.sh
+++ modules/getting-at-the-door/run.sh
@@ -1,1 +1,5 @@
 #!/bin/sh
+name=$(cat /dev/urandom | tr -dc a-zA-Z0-9 | head -c8)
+docker-compose -f /docker-compose.yml -p $name up -d
+docker-compose -f /docker-compose.yml -p $name exec webshell
+docker-compose -f /docker-compose.yml -p $name down
