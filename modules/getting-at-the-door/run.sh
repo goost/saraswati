ce30441fdc4b0653ef6de9df58316a07f847e201
Gleb Ostrowski <glost@mailbox.org>
2020-09-28 03:00:48 +0200

    feat(dockerprtwo): ✨ Working compose in container
    Alas, the ressources are not cleaned properly yet again.
    The need for an actual program handling life cycle
    is more and more apparent.

--- modules/getting-at-the-door/run.sh
+++ modules/getting-at-the-door/run.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 name=$(cat /dev/urandom | tr -dc a-zA-Z0-9 | head -c8)
-docker-compose -f /docker-compose.yml -p $name up -d
-docker-compose -f /docker-compose.yml -p $name exec webshell
-docker-compose -f /docker-compose.yml -p $name down
+mkdir -p /tmp/$name
+cd /tmp/$name
+cat > docker-compose.yml << EOF
+services:
+  webshell:
+    restart: "no"
+    image: modules/getting-at-the-door
+    entrypoint: ['tail', '-f', '/dev/null']
+    depends_on:
+      - getter
+
+  getter:
+    image: alpine
+    runtime: runc
+    restart: "no"
+    entrypoint: ['sh', '-c', 'while true ; do wget -O- webshell:4296/?flag[3x73rn4l_kn0ck!ng] ; sleep 5 ; done']
+EOF
+# TODO (glost) Ressources are not cleaned up
+trap 'docker-compose down ; cd / ; rm -rf /tmp/$name' 1 3 9
+docker-compose up -d
+docker-compose exec webshell /usr/bin/entrypoint.sh sh
+#docker-compose down
+#cd /
+#rm -rf /tmp/$name
